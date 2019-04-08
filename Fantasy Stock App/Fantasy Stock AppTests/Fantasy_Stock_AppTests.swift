43103e66054fcb64d0d80a3e574fc77adbf7db21
mattwang0 <34781925+mattwang0@users.noreply.github.com>
2019-04-08 16:38:03 -0400

    Add files via upload

---
+++ Fantasy Stock App/Fantasy Stock AppTests/Fantasy_Stock_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Fantasy_Stock_AppTests.swift
+//  Fantasy Stock AppTests
+//
+//  Created by Matt W on 4/8/19.
+//  Copyright © 2019 Group 4. All rights reserved.
+//
+
+import XCTest
+@testable import Fantasy_Stock_App
+
+class Fantasy_Stock_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
