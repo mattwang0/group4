ad9d32f62f03f704fd8113eacb98b944cce76d47
Matt W <matt@matts-mbp.wireless.davidson.edu>
2019-05-01 14:28:56 -0400

    the changes to webapp patrick sent me around 2:30 wednesday

--- welcome.php
+++ welcome.php
@@ -27,7 +27,29 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
     </p>
+    <form action="/stockSearch.php" method="GET">
+      <fieldset>
+        <legend>Search For a Stock</legend>
+        Stock Ticker:<br>
+        <input type="text" name="ticker" id = "ticker">
+        <br>
+        <input type="submit" value="Submit">
+      </fieldset>
+    </form>
 
-    <script type="text/javascript" src = "welcome.js"></script>
+    <script type="text/javascript">
+        var request = new XMLHttpRequest();
+        
+        var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' //+ ticker + '&interval=5min&outputsize=full&apikey=demo';
+        request.open('GET', 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&outputsize=full&apikey=demo', true);
+
+        request.onload = function() {
+        // Begin accessing JSON data here
+        var data = JSON.parse(this.response);
+        console.log(data);
+        }
+
+        request.send();
+    </script>
 </body>
 </html>
