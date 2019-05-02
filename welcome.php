da7e1f6b577cdef19233026d3c207fadfc5bb3bb
Patrick Kelly Jr <pkelly@Antoniettes-MacBook-Air.local>
2019-05-01 22:46:20 -0400

    I was able to upate the welcome page to show the current portfolio...I haven't messed with formatting yet

--- welcome.php
+++ welcome.php
@@ -27,7 +27,63 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
     </p>
+    <form action="/stockSearch.php" method="GET">
+      <fieldset>
+        <legend>Search For a Stock</legend>
+        Stock Ticker:<br>
+        <input type="text" name="ticker" id = "ticker">
+        <br>
+        <input type="submit" value="Submit">
+      </fieldset>
+    </form>
 
-    <script type="text/javascript" src = "welcome.js"></script>
+    <head>
+    <style>
+    table, th, td {
+      border: 1px solid black;
+    }
+    </style>
+    </head>
+    
+    <table align="center">
+      <caption><?php echo $_SESSION["username"];?> Portfolio</caption>
+      <tr>
+        <th>Stock</th>
+        <th>Purchase Price</th>
+        <!--<th>Current Price</th>
+        <th>Margin</th>-->
+      </tr>
+      <tr>
+
+
+
+        <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "PKswims83";
+            $dbname = "stockSchema";
+
+            mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+            $conn = new mysqli($servername, $username, $password);
+
+            mysqli_select_db($conn, $dbname) or die("Could not open the '$dbname'");
+
+            $user = $_SESSION["username"];
+
+            $query = "SELECT * FROM owns WHERE username = '$user'";
+            $result = mysqli_query($conn, $query);
+
+
+            while($row = mysqli_fetch_array($result)) {
+                echo 
+                    "<tr>
+                        <td> $row[1]</td><td> $row[2]</td>
+                    </tr>";
+
+            }
+
+        ?>
+    </table>
 </body>
 </html>
