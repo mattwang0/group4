e76dd81b7fa9fa4694213b70ea714c7c136bd342
Matt W <matt@matts-mbp.wireless.davidson.edu>
2019-05-01 20:55:39 -0400

    add stock working

--- stockSearch.php
+++ stockSearch.php
@@ -5,26 +5,29 @@
 
   <?php
 
-
-    $ticker = $_GET["ticker"];
-    $urlFirst = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=';
-    $urlSecond = '&interval=5min&outputsize=full&apikey=E1TND8XYQA8S7DLQ';
-    $url = $urlFirst . $ticker . $urlSecond;
-  
-
-    $data = file_get_contents($url);
-    $data = substr($data,65);
-    $startmark = strpos($data, "open");
-    $currentPrice = substr($data,$startmark+8,9);
-    $currentPrice = preg_replace('/[^0-9.]/', '', $currentPrice);
+  // Initialize the session
+  session_start();
 
 
-    // echo $currentPrice;
-    // echo "<br>";
-    // echo $data;
+  $ticker = $_GET["ticker"];
+  $urlFirst = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=';
+  $urlSecond = '&interval=5min&outputsize=full&apikey=E1TND8XYQA8S7DLQ';
+  $url = $urlFirst . $ticker . $urlSecond;
 
-    $row = explode("\n",$data);
-    $day = explode(",",$row[1]);
+
+  $data = file_get_contents($url);
+  $data = substr($data,65);
+  $startmark = strpos($data, "open");
+  $currentPrice = substr($data,$startmark+8,9);
+  $currentPrice = preg_replace('/[^0-9.]/', '', $currentPrice);
+
+
+  // echo $currentPrice;
+  // echo "<br>";
+  // echo $data;
+
+  $row = explode("\n",$data);
+  $day = explode(",",$row[1]);
 
     
   ?>
@@ -33,20 +36,50 @@
 
 
   <?php
-  $servername = "localhost";
-  $username = "root";
-  $password = "123456";
-  $dbname = "stockSchema";
+  // IF ADD STOCK IS CLICKED, CONNECT TO DATABASE AND ADD CURRENT TICKER AND PRICE TO DATABASE
+  if (isset($_POST["addStock"])) {
+    $user = $_SESSION['username']; //retrieve username from last page
 
-  $conn = new mysqli($servername, $username, $password);
+    $servername = "localhost";
+    $username = "root";
+    $password = "123456";
+    $dbname = "stockSchema";
 
-  mysqli_select_db($conn, $dbname) or die("Could not open the '$dbname'");
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+  
+    $conn = new mysqli($servername, $username, $password);
+  
+    mysqli_select_db($conn, $dbname) or die("Could not open the '$dbname'");
+    $user = mysqli_real_escape_string($conn, $user);
+    $ticker = mysqli_real_escape_string($conn, $ticker);
+    $insertQuery = "INSERT INTO owns (username, stock, price) VALUES ('$user', '$ticker', '$currentPrice')";
+    // $insertQuery = "INSERT INTO owns (username, stock, price) VALUES ('mawang', '', 123.45)";
+    // $result = ;
+    try {
+      if (mysqli_query($conn, $insertQuery)) {
+        // confirmation alert
+        echo '<script language="javascript">';
+        echo 'alert("Stock successfully added to portfolio")';
+        echo '</script>';
+      } else {
+        // bad alert
+        echo '<script language="javascript">';
+        echo 'alert("Something went wrong!")';
+        echo '</script>';
+      }
+    }
+    catch (Exception $e) {
+      // bad alert
+      echo 'Caught exception: ',  $e->getMessage(), "\n";
+    }
+  }
+
   ?>
 
 
   <div class="form-group">
-    <form action="" method="post">
-      <input type="submit" class="btn btn-primary" value="Add to portfolio">
+    <form action="/stockSearch.php?ticker=<?php echo $ticker?>" method="post">
+      <input type="submit" name="addStock" class="btn btn-primary" value="Add to portfolio">
     </form>
     <form action="/welcome.php" method="post">
       <input type="submit" class="btn btn-primary" value="Go back">
